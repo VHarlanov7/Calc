b538f2e1a563e1124d57908b12446b7f8436309b
VHarlanov7 <vharlanov7@gmail.com>
2022-01-12 12:28:12 +0300

    1.0

---
+++ src/Calc.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Calc {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String moyText = scan.nextLine();
+
+
+        String zavhoz = "пУПКИН";
+        int vozrast = 50;
+
+        System.out.println(moyText+zavhoz+" "+vozrast);
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
